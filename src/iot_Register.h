cbc2f362fa5e3bd250ef62c18588879efd12809f
Nathan Genetzky <nathan@genetzky.us>
2018-11-14 00:13:52 -0600

    iot_Register: Easy to share integers between device and cloud.
    Imported from:
    https://github.com/NGenetzky/particle-projects/commit/14785a16b889e30b72f3477a2f9bb25a3184d4dd
    
    History
    https://github.com/NGenetzky/particle-projects/tree/14785a16b889e30b72f3477a2f9bb25a3184d4dd/libraries/iot_Register
    → glog libraries/iot_Register/
    * 14785a1 ngenetzky: iot_Register_0.1.1: Added DigitalPortRegister. -   (1 year, 2 months ago)
    * 14bf0c2 ngenetzky: iot_Tinker: TinkerDigital Example finished. -   (1 year, 2 months ago)
    * 2761107 ngenetzky: iot_0.1.0: Add iot_Register. -   (1 year, 2 months ago)
    * 92f19b4 ngenetzky: iot_Register: Finish LEDStatusRegister example. -   (1 year, 2 months ago)
    * ec0e521 ngenetzky: iot_Register: Finish TimeRegister example. -   (1 year, 2 months ago)
    * 1f853d9 ngenetzky: iot_DigitalRegister: Finished DigitalPort example. -   (1 year, 2 months ago)
    * 34c6d5f ngenetzky: iot_DigitalRegister: Add DigitalPort. Needs changes. -   (1 year, 2 months ago)
    * b47c696 ngenetzky: iot_Register: Added DigitalPin. -   (1 year, 2 months ago)

---
+++ src/iot_Register.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/*
+  iot_Register library by ngenetzky
+ */
+
+#include "iot/Pin.h"
+#include "iot/DigitalPin.h"
+#include "iot/DigitalPort.h"
+
+#include "iot/Register.h"
+#include "iot/RegisterBank.h"
+
+#include "iot/LEDStatusRegister.h"
+#include "iot/DigitalPortRegister.h"
